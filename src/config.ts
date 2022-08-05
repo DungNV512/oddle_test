27e1e67df0df675a64823d5c38ff946251e5efb1
DungNV512 <dungnv.512@gmail.com>
2022-08-05 17:01:33 +0700

    fix: missing token

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiEndpoint: 'https://api.github.com/',
   graphqlUri: 'https://api.github.com/graphql',
-  githubToken: process.env.GITHUB_TOKEN || ''
+  githubToken: process.env.GITHUB_TOKEN || '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'
 }
 
 export default config
