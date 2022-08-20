a722689e751b8340f0171e4b16741da14d05f933
Di Envy <dungnv.512@gmail.com>
2022-08-20 13:37:51 +0700

    revoke github token

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiEndpoint: 'https://api.github.com/',
   graphqlUri: 'https://api.github.com/graphql',
-  githubToken: process.env.GITHUB_TOKEN || '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'
+  githubToken: process.env.GITHUB_TOKEN || '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'
 }
 
 export default config
